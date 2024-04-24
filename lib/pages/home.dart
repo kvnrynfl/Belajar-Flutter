af312e2f6728d183d7202bfbcc58c2dde7573f5a
Kevin Reynaufal <kevinreynaufal2004@gmail.com>
2024-04-24 16:10:58 +0700

    fix homepage and add card for uts

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:belajar_flutter/pages/pertemuan_01/main.dart';
 import 'package:belajar_flutter/pages/pertemuan_02/main.dart';
 import 'package:belajar_flutter/pages/pertemuan_03/main.dart';
@@ -5,10 +6,24 @@ import 'package:belajar_flutter/pages/pertemuan_04/main.dart';
 import 'package:belajar_flutter/pages/pertemuan_05/main.dart';
 import 'package:belajar_flutter/pages/pertemuan_06/main.dart';
 import 'package:belajar_flutter/pages/pertemuan_07/login_page.dart';
-import 'package:flutter/material.dart';
+import 'package:belajar_flutter/pages/ujian_tengah_semester/main.dart';
+import 'package:belajar_flutter/pages/ujian_tengah_semester/static_data.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late StaticData userData;
+
+  @override
+  void initState() {
+    super.initState();
+    userData = StaticData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,70 +33,58 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Center(
         child: ListView(
-          children: const [
-            CardItem(
+          children: [
+            _buildCardItem(
+              title: "Ujian Tengah Semester",
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: UjianTengahSemester(userData: userData),
+            ),
+            _buildCardItem(
               title: "Pertemuan 1",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan01(title: "Pertemuan 1"),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan01(title: "Pertemuan 1"),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 2",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan02(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan02(),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 3",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan03(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan03(),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 4",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan04(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan04(),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 5",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan05(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan05(),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 6",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan06(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan06(),
             ),
-            CardItem(
+            _buildCardItem(
               title: "Pertemuan 7",
-              description:
-                  "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
-              destination: Pertemuan07(),
+              description: "Cupidatat non nostrud consequat qui qui cupidatat sunt ipsum ullamco consequat.",
+              destination: const Pertemuan07(),
             ),
           ],
         ),
       ),
     );
   }
-}
 
-class CardItem extends StatelessWidget {
-  const CardItem({
-    super.key,
-    required this.title,
-    required this.description,
-    required this.destination,
-  });
-
-  final String title;
-  final String description;
-  final Widget destination;
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildCardItem({
+    required String title,
+    required String description,
+    required Widget destination,
+  }) {
     return InkWell(
       onTap: () {
         Navigator.push(
